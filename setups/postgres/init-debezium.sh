684e000d600bbfea225b433bcc6a2d92343548ab
co <cockernutx@gmail.com>
2025-07-10 11:12:49 -0300

    Removed some init scripts

--- setups/postgres/init-debezium.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-export DEBEZIUM_PASSWORD="${DEBEZIUM_PASSWORD:-debezium_pass}"
-sed "s/\\${DEBEZIUM_PASSWORD}/$DEBEZIUM_PASSWORD/g" /docker-entrypoint-initdb.d/10-global.sql.template > /docker-entrypoint-initdb.d/10-global.sql
--- setups/postgres/init-per-db.sql
+++
@@ -1,6 +0,0 @@
-/* Template applied to each service DB */
-\c :DBNAME;
-GRANT CONNECT ON DATABASE :DBNAME TO debezium_user;
-GRANT USAGE ON SCHEMA public TO debezium_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO debezium_user;
-CREATE PUBLICATION IF NOT EXISTS dbz_publication FOR ALL TABLES;
