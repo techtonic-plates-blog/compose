fa65781b0ebb5969a296c203b371fe9a18c1b0f2
co <cockernutx@gmail.com>
2025-07-11 14:21:07 -0300

    Updated microservices compose files

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,5 @@
-MINIO_ACCESS=YX0Q15RENVBF6FQZZ8BD MINIO_SECRET=0fC0ypCecVIwiGZn68l4+8r5nwzv89Rdgf8aBbas podman compose -f compose.posts.yaml -p posts-service up --build --force-recreate -d;
-MINIO_ACCESS=YX0Q15RENVBF6FQZZ8BD MINIO_SECRET=0fC0ypCecVIwiGZn68l4+8r5nwzv89Rdgf8aBbas podman compose -f compose.assets.yaml -p assets-service up --build --force-recreate -d;
+podman compose -f compose.posts.yaml -p posts-service up --build --force-recreate -d;
+podman compose -f compose.assets.yaml -p assets-service up --build --force-recreate -d;
 
 podman compose -f compose.auth.yaml -p auth-service up --build --force-recreate -d;
 podman compose -f compose.infrastructure.yaml up --build --force-recreate -d;
