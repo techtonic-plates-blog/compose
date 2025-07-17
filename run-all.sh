08629aa6c5acab91dbb6c7b3fbaee256b2828f47
co <cockernutx@gmail.com>
2025-07-17 12:14:49 -0300

    Minor fixes

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,7 @@
+
+podman compose -f compose.infrastructure.yaml up --build --force-recreate -d;
+
 podman compose -f compose.posts.yaml -p posts-service up --build --force-recreate -d;
 podman compose -f compose.assets.yaml -p assets-service up --build --force-recreate -d;
 
 podman compose -f compose.auth.yaml -p auth-service up --build --force-recreate -d;
-podman compose -f compose.infrastructure.yaml up --build --force-recreate -d;
