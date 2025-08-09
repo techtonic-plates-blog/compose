eb00150bb56a50492717f0c77e08c0ed7fe2aa41
co <cockernutx@gmail.com>
2025-08-09 16:19:05 -0300

    Now merged all compose and envs into a single file

--- run-all.sh
+++
@@ -1,8 +0,0 @@
-
-podman compose -f compose.infrastructure.yaml up --build --force-recreate -d;
-
-podman compose -f compose.posts.yaml -p posts-service up --build --force-recreate -d;
-podman compose -f compose.assets.yaml -p assets-service up --build --force-recreate -d;
-
-podman compose -f compose.auth.yaml -p auth-service up --build --force-recreate -d;
-podman compose -f compose.dashboard.yaml -p dashboard-service up --build --force-recreate -d;
